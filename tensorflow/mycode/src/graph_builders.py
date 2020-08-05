3e6642dfaae0b5075b461f851ecd145901222392
christinazavou <czavou01@gmail.com>
2020-08-05 16:38:55 +0300

    looking at convolution shapes

--- tensorflow/mycode/src/graph_builders.py
+++ tensorflow/mycode/src/graph_builders.py
@@ -12,6 +12,9 @@ def autoencoder_graph():
     pass
 
 
+channels = [512, 256, 128, 64, 32, 16, 8, 4, 2]  # convolutions set for up to depth 8
+
+
 def octree_conv_bn_relu(data, octree, depth, channel, training, kernel_size=[3],
                         stride=1, fast_mode=False):
     with tf.variable_scope('conv_bn_relu'):
@@ -47,34 +50,54 @@ def batch_norm(inputs, training, axis=1):
     return tf.layers.batch_normalization(inputs, axis=axis, training=training)
 
 
+def ocnn_encoder(octree, depth, in_channel, training=True, reuse=None, debug=False):
+    assert depth > 1 and depth < 9, "Depth {} can't be used.".format(depth)
+
+    debug_checks = {}
+
+    with tf.variable_scope("ocnn_encoder", reuse=reuse):
+        data = octree_property(octree, property_name="feature", dtype=tf.float32,
+                               depth=depth, channel=in_channel)
+        data = tf.reshape(data, [1, in_channel, -1, 1])
+        if debug:
+            debug_checks['input data reshaped: {}'] = data
+
+        for d in range(depth, 2, -1):
+            with tf.variable_scope('depth_%d' % d):
+                data = octree_conv_bn_relu(data, octree, d, channels[d], training)
+                if debug:
+                    debug_checks['output of conv_d{}:'.format(d)] = data
+                data, _ = octree_max_pool(data, octree, d)
+                if debug:
+                    debug_checks['output of pool_d{}:'.format(d)] = data
+
+        with tf.variable_scope("full_voxel"):
+            data = octree_full_voxel(data, depth=2)
+            if debug:
+                debug_checks['output of full_voxel:'] = data
+            data = tf.layers.dropout(data, rate=0.5, training=training)
+    return data, debug_checks
+
+
+def ocnn_classification_logit(encoded_data, n_out, training=True, reuse=None, debug=False):
+    debug_checks = {}
+    with tf.variable_scope("ocnn_classifier", reuse=reuse):
+        with tf.variable_scope("fc1"):
+            data = fc_bn_relu(encoded_data, channels[2], training=training)
+            if debug:
+                debug_checks['output of fc_bn_relu:'] = data
+            data = tf.layers.dropout(data, rate=0.5, training=training)
+
+        with tf.variable_scope("fc2"):
+            _logit = dense(data, n_out, use_bias=True)
+            if debug:
+                debug_checks['output shape:'] = _logit
+    return _logit, debug_checks
+
+
 def classification_graph(octree, label, flags, training=True, reuse=None):
-    def ocnn_logit():
-        depth = flags.depth
-        channels = [512, 256, 128, 64, 32, 16, 8, 4, 2]
-        with tf.variable_scope("ocnn_encoder", reuse=reuse):
-            data = octree_property(octree, property_name="feature", dtype=tf.float32,
-                                   depth=depth, channel=flags.channel)
-            data = tf.reshape(data, [1, flags.channel, -1, 1])
-
-            for d in range(depth, 2, -1):
-                with tf.variable_scope('depth_%d' % d):
-                    data = octree_conv_bn_relu(data, octree, d, channels[d], training)
-                    data, _ = octree_max_pool(data, octree, d)
-
-            with tf.variable_scope("full_voxel"):
-                data = octree_full_voxel(data, depth=2)
-                data = tf.layers.dropout(data, rate=0.5, training=training)
-
-        with tf.variable_scope("ocnn_classifier", reuse=reuse):
-            with tf.variable_scope("fc1"):
-                data = fc_bn_relu(data, channels[2], training=training)
-                data = tf.layers.dropout(data, rate=0.5, training=training)
-
-            with tf.variable_scope("fc2"):
-                _logit = dense(data, flags.nout, use_bias=True)
-        return _logit
-
-    logit = ocnn_logit()
+    encoded_data, _ = ocnn_encoder(octree, flags.depth, flags.channel, training, reuse)
+    logit, _ = ocnn_classification_logit(encoded_data, flags.nout, training, reuse)
     # prediction = tf.nn.softmax(logit)  # tf.argmax(logit, axis=1, output_type=tf.int32)
 
     trainables = GraphAccess.get_variables([], [], True, True)
--- tensorflow/mycode/test/test_graph_builders.py
+++ tensorflow/mycode/test/test_graph_builders.py
@@ -1,6 +1,6 @@
 from src.data_parsing import DatasetFactory
-from test.helper import mock_config
 from src.graph_builders import *
+from test.helper import mock_config
 
 
 class DatasetTest(tf.test.TestCase):
@@ -15,30 +15,21 @@ class DatasetTest(tf.test.TestCase):
         flags_data_train = mock_config().DATA.train
         depth = 5
         channel = 3
-        channels = [512, 256, 128, 64, 32, 16, 8, 4, 2]
-        training= True
 
         octree, label = DatasetFactory(flags_data_train)()
 
-        data_check = {}
-
-        with tf.variable_scope("ocnn_encoder", reuse=False):
-            data = octree_property(octree, property_name="feature", dtype=tf.float32,
-                                   depth=depth, channel=channel)
-            data = tf.reshape(data, [1, channel, -1, 1])
-            data_check['feature_reshaped'] = data
-
-            for d in range(depth, 2, -1):
-                with tf.variable_scope('depth_%d' % d):
-                    data = octree_conv_bn_relu(data, octree, d, channels[d], training)
-                    data_check['conv_depth{}'.format(d)] = data
-                    data, _ = octree_max_pool(data, octree, d)
-                    data_check['pool_depth{}'.format(d)] = data
+        encoded_data, debug_checks = ocnn_encoder(octree, depth, channel, True, False, True)
+        _, debug_checks_fc = ocnn_classification_logit(encoded_data, 40, True, False, True)
 
         with tf.Session() as sess:
             sess.run(tf.global_variables_initializer())
-            data_check_results = sess.run(data_check)
-            print("data_check_results:", {key: value.shape for key, value in data_check_results.items()})
+            debug_checks_shapes, debug_checks_fc_shapes = sess.run([debug_checks, debug_checks_fc])
+            print("\ndebug_checks_shapes:\n")
+            for key, value in debug_checks_shapes.items():
+                print("{}: {}".format(key, value.shape))
+            print("\ndebug_checks_fc_shapes:\n")
+            for key, value in debug_checks_fc_shapes.items():
+                print("{}: {}".format(key, value.shape))
 
 
 if __name__ == "__main__":
