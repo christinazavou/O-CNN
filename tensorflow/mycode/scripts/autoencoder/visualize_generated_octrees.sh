0dd0f63f613eba80bdf06b65a7151e49b7f398e2
christinazavou <czavou01@gmail.com>
2020-08-13 15:36:12 +0300

    fixed and automated the check of autoencoder

--- tensorflow/mycode/scripts/autoencoder/visualize_generated_octrees.sh
+++ tensorflow/mycode/scripts/autoencoder/visualize_generated_octrees.sh
@@ -2,9 +2,7 @@
 
 cd /home/christina/Documents/ANNFASS_code/zavou-repos/O-CNN/octree/build
 
-#logs_path="/home/christina/Documents/ANNFASS_code/zavou-repos/O-CNN/tensorflow/script/logs/ae/ocnn_b16_decode_shape"
-logs_path="/home/christina/Documents/ANNFASS_code/zavou-repos/O-CNN/tensorflow/script/logs/ae/resnet_b16_decode_shape"
+logs_path=${1}
 
 ./octree2mesh --filenames ${logs_path}/input_shapes.txt --output_path ${logs_path}/inputshapes
-
 ./octree2mesh --filenames ${logs_path}/output_shapes.txt --output_path ${logs_path}/outputshapes
