d2439a43c57bc82f029427288e5c3c47469f53f6
christinazavou <czavou01@gmail.com>
2020-09-01 13:27:07 +0300

    debugging default building points

--- tensorflow/data/seg_buildings.py
+++ tensorflow/data/seg_buildings.py
@@ -1,43 +1,144 @@
 import os
-
+import json
+import numpy as np
 
 current_path = os.path.dirname(os.path.realpath(__file__))
 convert_tfrecords = os.path.join(current_path, '../util/convert_tfrecords.py')
 
+all_categoty = ['Bag', 'Bed', 'Bottle', 'Bowl', 'Chair', 'Clock', 'Dishwasher',
+                'Display', 'Door', 'Earphone', 'Faucet', 'Hat', 'Keyboard',
+                'Knife', 'Lamp', 'Laptop', 'Microwave', 'Mug', 'Refrigerator',
+                'Scissors', 'StorageFurniture', 'Table', 'TrashCan', 'Vase']
+finegrained_category = ['Bed', 'Bottle', 'Chair', 'Clock', 'Dishwasher', 'Display',
+                        'Door', 'Earphone', 'Faucet', 'Knife', 'Lamp', 'Microwave',
+                        'Refrigerator', 'StorageFurniture', 'Table', 'TrashCan', 'Vase']
+level_list_list = [[1], [1, 2, 3], [1, 3], [1], [1, 2, 3], [1, 3], [1, 2, 3],
+                   [1, 3], [1, 2, 3], [1, 3], [1, 3], [1], [1], [1, 3],
+                   [1, 2, 3], [1], [1, 2, 3], [1], [1, 2, 3], [1], [1, 2, 3],
+                   [1, 2, 3], [1, 3], [1, 3]]
+finest_level = [1, 3, 3, 1, 3, 3, 3, 3, 3, 3,
+                3, 1, 1, 3, 3, 1, 3, 1, 3, 1, 3, 3, 3, 3]
+finest_level_dict = dict(zip(all_categoty, finest_level))
+level_list_dict = dict(zip(all_categoty, level_list_list))
+
+
+def from_colored_annotated_data_to_default_ply(sample_pts=None):
+  data_path = '/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/w_colour_norm_w_labels'
+  suffix = 'ply100000' if sample_pts is None else 'ply'+str(sample_pts)
+  output_path = '/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/'+suffix
+  print('Convert the raw data to ply files ...')
+  if not os.path.exists(output_path):
+    os.makedirs(output_path)
+  for annotated_file in os.listdir(data_path):
+    annotation_id = annotated_file.replace('_w_label.txt', '')
+    from_colored_single_data_to_default_ply(os.path.join(data_path, annotated_file),
+                                            os.path.join(output_path, annotation_id + ".ply"),
+                                            sample_pts)
+
+
+def from_colored_single_data_to_default_ply(input_file, output_file, sample_pts=None):
+  a = np.loadtxt(input_file)
+  if sample_pts:
+    sample_indices = np.random.randint(0, a.shape[0], sample_pts)
+    points = a[sample_indices, 0:3]
+    normals = a[sample_indices, 3:6]
+    labels = a[sample_indices, 10].astype(np.int)
+  else:
+    points = a[:, 0:3]
+    normals = a[:, 3:6]
+    labels = a[:, 10].astype(np.int)
+  save_ply(output_file, points, normals, labels)
+
+
+def convert_points(sample_pts=None, max_files=-1):
+  root_folder = '/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour'
+  input_folder = os.path.join(root_folder, 'ply100000' if sample_pts is None else 'ply' + str(sample_pts))
+  suffix = 'points100000' if sample_pts is None else 'points' + str(sample_pts)
+  suffix = suffix if max_files == -1 else str(max_files) + suffix
+  output_folder = os.path.join(root_folder, suffix)
+  print('Convert ply files to points files ...')
+  if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+  filenames = []
+  for filename in os.listdir(input_folder):
+    if filename.endswith('.ply'):
+      filenames.append(os.path.join(input_folder, filename))
+    if max_files != -1 and len(filenames) >= max_files:
+        break
+
+  chunk = 0
+  list_filenames = []
+  cmds = []
+  for filenames_chunk in chunks(filenames, int(len(filenames)/4)):
+    print(chunk)
+    chunk += 1
+    list_filename = os.path.join(output_folder, 'filelist_ply_chunk{}.txt'.format(chunk) )
+    list_filenames.append(list_filename)
+    with open(list_filename, 'w') as fid:
+      fid.write('\n'.join(filenames_chunk))
+
+    cmds.append(' '.join(['/home/christina/Documents/ANNFASS_code/zavou-repos/O-CNN/octree/build/./ply2points',
+          '--filenames', list_filename,
+          '--output_path', output_folder,
+          '--verbose', '0']))
+  for cmd in cmds:
+    print(cmd + "\n")
+
+
+def convert_points_to_tfrecords(sample_pts=None, max_files=-1):
+  root_folder = '/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour'
+  suffix = 'points100000' if sample_pts is None else 'points' + str(sample_pts)
+  suffix = suffix if max_files == -1 else str(max_files) + suffix
+  data_path = os.path.join(root_folder, suffix)
+
+  for phase in ["train", "test"]:
+    output_record = "{}_{}.tfrecords".format(phase, suffix)
+    output_record = os.path.join(root_folder, "dataset_points", output_record)
+    filelist_in = os.path.join(root_folder, "dataset_points", "_{}_points.txt".format(phase))
+    filelist_out = os.path.join(root_folder, "dataset_points", "_{}_{}.txt".format(phase, suffix))
+
+    with open(filelist_in, "r") as fin, open(filelist_out, "w") as fout:
+      for line in fin.readlines():
+        filename, label = line.split(" ")
+        filename = os.path.split(filename)[1]
+        if os.path.exists(os.path.join(data_path, filename)):
+          fout.write(filename + " " + label)
+
+    shuffle = '--shuffle true' if phase == 'train' else ''
+    cmds = ['python', convert_tfrecords,
+              '--file_dir', data_path,
+              '--list_file', filelist_out,
+              '--records_name', output_record,
+              shuffle]
+
+    cmd = ' '.join(cmds)
+    print(cmd)
+    os.system(cmd)
+
+
+def save_ply(filename, points, normals, labels):
+  assert points.shape[0] == normals.shape[0] == labels.shape[0]
+  assert points.shape[1] == normals.shape[1] == 3
+  labels = labels.reshape((points.shape[0], 1))
+  data = np.concatenate([points, normals, labels], axis=1)
+
+  header = "ply\nformat ascii 1.0\nelement vertex %d\n" \
+      "property float x\nproperty float y\nproperty float z\n" \
+      "property float nx\nproperty float ny\nproperty float nz\n" \
+      "property float label\nelement face 0\n" \
+      "property list uchar int vertex_indices\nend_header\n"
+  with open(filename, 'w') as fid:
+    fid.write(header % points.shape[0])
+    np.savetxt(fid, data, fmt='%.6f')
+
+
+def chunks(l, n):
+  n = max(1, n)
+  return (l[i:i+n] for i in range(0, len(l), n))
+
 
 if __name__ == '__main__':
-  # cmds = ['python', convert_tfrecords,
-  #         '--file_dir', '/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/ocnn_points',
-  #         '--list_file', "/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/dataset/test_points.txt",
-  #         '--records_name', "/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/dataset/test_points.tfrecords",
-  #         '']
-  # cmd = ' '.join(cmds)
-  # print(cmd)
-  # os.system(cmd)
-  #
-  # cmds = ['python', convert_tfrecords,
-  #         '--file_dir', '/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/ocnn_points',
-  #         '--list_file', "/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/dataset/train_points.txt",
-  #         '--records_name', "/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/dataset/train_points.tfrecords",
-  #         '--shuffle true']
-  # cmd = ' '.join(cmds)
-  # print(cmd)
-  # os.system(cmd)
-
-    cmds = ['python', convert_tfrecords,
-            '--file_dir', '/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/octrees_d7',
-            '--list_file', "/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/dataset/test_octrees.txt",
-            '--records_name', "/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/dataset/test_octrees.tfrecords",
-            '']
-    cmd = ' '.join(cmds)
-    print(cmd)
-    os.system(cmd)
-
-    cmds = ['python', convert_tfrecords,
-            '--file_dir', '/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/octrees_d7',
-            '--list_file', "/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/dataset/train_octrees.txt",
-            '--records_name', "/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/dataset/train_octrees.tfrecords",
-            '--shuffle true']
-    cmd = ' '.join(cmds)
-    print(cmd)
-    os.system(cmd)
+  # from_colored_annotated_data_to_default_ply()  #todo: run in parallel..
+  # convert_points()
+  convert_points_to_tfrecords(max_files=200)
---
+++ tensorflow/script/debug_seg_building_dataset.py
@@ -0,0 +1,178 @@
+import sys
+
+sys.path.append("..")
+from libs import *
+from dataset import *
+
+categories = [
+    'Bag', 'Bed', 'Bottle', 'Bowl', 'Chair', 'Clock', 'Dishwasher',
+    'Display', 'Door', 'Earphone', 'Faucet', 'Hat', 'Keyboard',
+    'Knife', 'Lamp', 'Laptop', 'Microwave', 'Mug', 'Refrigerator',
+    'Scissors', 'StorageFurniture', 'Table', 'TrashCan', 'Vase'
+]
+labels = [
+    -1, 15, 9, -1,
+    39, 11, 7, 4,
+    5, 10, 12, -1,
+    -1, 10, 41, -1,
+    6, -1, 7, -1,
+    24, 51, 11, 6
+]
+take = [
+    -1, 37, 84, -1,
+    1217, 98, 51, 191,
+    51, 53, 132, -1,
+    -1, 77, 419, -1,
+    39, -1, 31, -1,
+    451, 1668, 63, 233
+]
+categories = ['Buildings']
+labels = [34],
+take = [200]
+
+
+def config_points_partnet(category="Bottle"):
+    filename = '/media/christina/Data/ANFASS_data/partnet_data/dataset/{}_train_level3.tfrecords' \
+        .format(category)
+    depth = 6
+    task = 'seg_points_partnet'
+
+    octrees = PointDataset(ParseExample(x_alias='data', y_alias='label'),
+                           NormalizePoints(),
+                           TransformPoints(distort=True, depth=depth, offset=0, axis='y',
+                                           scale=0.25, jitter=0.125, angle=[5, 5, 5],
+                                           uniform=True,
+                                           bounding_sphere=bounding_sphere),
+                           Points2Octree(depth=depth, node_dis=True))
+
+    return octrees, filename, depth, task
+
+
+def config_points_buildings():
+    filename = '/media/christina/Elements/ANNFASS_DATA/RGBA_uniform/with_colour/dataset_points/test_200points100000.tfrecords'
+    depth = 7
+    task = 'seg_points_buildings'
+    octrees = PointDataset(ParseExample(x_alias='data', y_alias='label'),
+                           NormalizePoints(),
+                           TransformPoints(distort=True, depth=depth, offset=0, axis='y',
+                                           scale=0.25, jitter=0.125, angle=[5, 5, 5],
+                                           uniform=True,
+                                           bounding_sphere=bounding_sphere),
+                           Points2Octree(depth=depth, node_dis=True, ))
+    return octrees, filename, depth, task
+
+
+class DatasetDebug:
+    channels = {
+        'seg_points_partnet': {
+            'split': 0, 'label': 1, 'feature': 4, 'index': 1, 'xyz': 1
+        },
+        'seg_points_buildings': {
+            'split': 0, 'label': 1, 'feature': 4, 'index': 1, 'xyz': 1
+        }
+    }
+
+    dtypes = {
+        'split': tf.float32,
+        'label': tf.float32,
+        'feature': tf.float32,
+        'index': tf.int32,
+        'xyz': tf.uint32
+    }
+
+    @staticmethod
+    def check(octree, property_name, max_depth, task, session):
+        for d in range(0, max_depth + 1):
+            DatasetDebug.check_d(octree, property_name, d, max_depth, task, session)
+        # if property_name == "label":
+        #     labels_max_depth = DatasetDebug.check_d(octree, property_name, max_depth, max_depth, task, session)
+        #     labels_minus_depth = DatasetDebug.check_d(octree, property_name, -1, max_depth, task, session)
+
+    @staticmethod
+    def check_d(octree, property_name, d, max_depth, task, session):
+        result = session.run(octree_property(octree, property_name=property_name, depth=d,
+                                             dtype=DatasetDebug.dtypes[property_name],
+                                             channel=DatasetDebug.channels[task][property_name]))
+        print("depth {} {} {}".format(d, property_name, result.shape))
+        assert result.shape[0] == DatasetDebug.channels[task][property_name]
+        assert d == -1 or result.shape[1] <= 8 ** d
+        # if property_name == "label" and 0 < d < max_depth and task == 'seg_points_partnet':
+        #     assert set(result.reshape((-1))) <= {-1, 1}  # if d=max_depth then we have label in (-1, #categories)
+        # return result
+
+    @staticmethod
+    def check_config(octree, octree5, depth, task, sess=None):
+        if sess is None:
+            sess = tf.Session()
+        DatasetDebug.check(octree, 'split', depth, task, sess)
+        DatasetDebug.check(octree, 'label', depth, task, sess)
+
+        # "feature" must be the input signal..i.e. in last depth is the nx,ny,nz and then in each preceding
+        # depth is the average of its children nodes
+        DatasetDebug.check(octree, 'feature', depth, task, sess)
+        DatasetDebug.check(octree, 'index', depth, task, sess)
+
+        # "xyz" is the shuffle key
+        DatasetDebug.check(octree, "xyz", depth, task, sess)
+
+        try:
+            DatasetDebug.check_d(octree5, "xyz", 0, depth, task, sess)
+        except:
+            pass  # more than one octrees merged thus more output rows in the result
+
+
+def check_properties():
+    # octrees, filename, depth, task = config_points_partnet()
+    octrees, filename, depth, task = config_points_buildings()
+    with tf.Session() as sess:
+        octree, _, points = sess.run(octrees(filename, batch_size=1, shuffle_size=0,
+                                             return_iter=True, take=10,
+                                             return_pts=True).get_next())
+        octree5, _, _ = sess.run(octrees(filename, batch_size=5, shuffle_size=0,
+                                         return_iter=True, take=10,
+                                         return_pts=True).get_next())
+        # DatasetDebug.check_config(octree, octree5, depth, task, sess)
+        points_xyzd = points_property(points, property_name='xyz', channel=4)
+        points_xyz = points_property(points, property_name='xyz', channel=3)
+        points_label = points_property(points, property_name='label', channel=1)
+        octree_xyz = octree_property(octree, property_name="xyz", depth=depth, dtype=tf.uint32, channel=1)
+        octree_label = octree_property(octree, property_name="label", depth=depth, dtype=tf.float32, channel=1)
+        pxyzd, pxyz, plabel, oxyz, olabel = sess.run([points_xyzd, points_xyz, points_label, octree_xyz, octree_label])
+        print("points_xyzd: ", pxyzd.shape)
+        print("points_xyz: ", pxyz.shape)
+        print("points_label: ", plabel.shape)
+        print("octree_xyz: ", oxyz.shape)
+        print("octree_label: ", olabel.shape)
+
+        # ----------------------------------------------------------------------------------
+
+        # for cat_idx in range(len(categories)):
+        #     minl = 100
+        #     maxl = -1
+        #     if labels[cat_idx] == -1:
+        #         continue
+        #
+        #     # octrees, filename, depth, task = config_points_partnet(categories[cat_idx])
+        #     octrees, filename, depth, task = config_points_buildings()
+        #     _, _, points = octrees(filename, batch_size=1, shuffle_size=0,
+        #                            return_iterator=False, take=take[cat_idx], return_pts=True)
+        #     points_label = points_property(points, property_name='label', channel=1)
+        #     points_xyzd = points_property(points, property_name='xyz', channel=4)
+        #
+        #     # points_label = points_property(octrees(filename, batch_size=1, shuffle_size=0,
+        #     #                                        return_iterator=False, take=take[cat_idx],
+        #     #                                        return_pts=True)[2], property_name='label',
+        #     #                                channel=1)
+        #     for i in range(take[cat_idx]):
+        #         pl, xyz = sess.run([points_label, points_xyzd])
+        #         minl = min(minl, pl.min())
+        #         maxl = max(maxl, pl.max())
+        #
+        #         if pl.shape[0] != 10000:
+        #             assert pl.shape[0] < 10000
+        #             assert pl.shape[0] == xyz.shape[0]
+        #
+        #     print(categories[cat_idx], minl, maxl)
+
+
+check_properties()
