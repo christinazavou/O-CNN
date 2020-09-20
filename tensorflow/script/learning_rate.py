bad048572af5633dbda39cd5b83937783fa203de
christinazavou <czavou01@gmail.com>
2020-09-20 21:12:32 +0300

    plateau with tensors

--- tensorflow/script/learning_rate.py
+++ tensorflow/script/learning_rate.py
@@ -26,27 +26,93 @@ class StepLR:
   def __call__(self, global_step):
     with tf.variable_scope('step_lr'):
       step_size = list(self.flags.step_size)
-      for i in range(len(step_size), 5): 
-        step_size.append(step_size[-1])
+      for i in range(len(step_size), 5):
+          step_size.append(step_size[-1])
 
       steps = step_size
       for i in range(1, 5):
-        steps[i] = steps[i-1] + steps[i]
-      lr_values = [self.flags.gamma**i * self.flags.learning_rate for i in range(0, 6)]
+          steps[i] = steps[i - 1] + steps[i]
+      lr_values = [self.flags.gamma ** i * self.flags.learning_rate for i in range(0, 6)]
 
       lr = tf.train.piecewise_constant(global_step, steps, lr_values)
     return lr
 
 
+class OnPlateauLR:
+    def __init__(self, flags):
+        self.flags = flags
+        self.best_metric = tf.Variable(initial_value=0.) if self.flags.mode == 'max' \
+            else tf.Variable(initial_value=1.e100)
+        self.prev_lr = tf.Variable(initial_value=self.flags.learning_rate)
+        self.cooldown_counter = tf.Variable(initial_value=0)
+        self.bad_epochs = tf.Variable(initial_value=0)
+
+    def is_better(self, curr_metric):
+        if self.flags.mode == 'min' and self.flags.threshold_mode == 'rel':
+            rel_epsilon = 1. - self.flags.threshold
+            return tf.less(curr_metric, tf.multiply(self.best_metric, rel_epsilon))
+
+        elif self.flags.mode == 'min' and self.flags.threshold_mode == 'abs':
+            return tf.less(curr_metric, tf.subtract(self.best_metric, self.flags.threshold))
+
+        elif self.flags.mode == 'max' and self.flags.threshold_mode == 'rel':
+            rel_epsilon = self.flags.threshold + 1.
+            return tf.greater(curr_metric, tf.multiply(self.best_metric, rel_epsilon))
+
+        else:  # mode == 'max' and epsilon_mode == 'abs':
+            return tf.greater(curr_metric, tf.add(self.best_metric, self.flags.threshold))
+
+    def __call__(self, curr_metric):
+        # new_lr = tf.Variable(initial_value=0)
+        with tf.variable_scope('plateau_lr'):
+            self.bad_epochs, self.best_metric = tf.cond(
+                self.is_better(curr_metric),
+                true_fn=lambda: [tf.subtract(self.bad_epochs, self.bad_epochs), curr_metric],
+                false_fn=lambda: [tf.add(self.bad_epochs, 1), self.best_metric]
+            )
+
+            self.bad_epochs, self.cooldown_counter = tf.cond(
+                tf.greater(self.cooldown_counter, 0),
+                true_fn=lambda: [0, tf.add(self.cooldown_counter, -1)],
+                false_fn=lambda: [self.bad_epochs, self.cooldown_counter]
+            )
+
+            new_lr, self.cooldown_counter, self.bad_epochs = tf.cond(
+                tf.greater(self.bad_epochs, self.flags.patience),
+                true_fn=lambda: [tf.maximum(tf.multiply(self.prev_lr, self.flags.gamma), self.flags.min_lr),
+                                 self.flags.cooldown,
+                                 0],
+                false_fn=lambda: [self.prev_lr, self.cooldown_counter, self.bad_epochs]
+            )
+
+            new_lr = tf.cond(tf.greater(tf.subtract(self.prev_lr, new_lr), self.flags.eps),
+                             true_fn=lambda: new_lr,
+                             false_fn=lambda: self.prev_lr)
+            self.prev_lr = new_lr
+            return new_lr
+
+
+# class LRFactory:
+#     def __init__(self, flags):
+#         self.flags = flags
+#         if self.flags.lr_type == 'plateau':
+#             self.lr = OnPlateauLR(flags)
+#         else:
+#             print('Error, unsupported learning rate: ' + self.flags.lr_type)
+#
+#     def __call__(self, curr_metric, current_lr):
+#         return self.lr(curr_metric, current_lr)
+
+
 class LRFactory:
-  def __init__(self, flags):
-    self.flags = flags
-    if self.flags.lr_type == 'step':
-      self.lr = StepLR(flags)
-    elif self.flags.lr_type == 'cos':
-      self.lr = CosLR(flags)
-    else:
-      print('Error, unsupported learning rate: ' + self.flags.lr_type)
-  
-  def __call__(self, global_step):
-    return self.lr(global_step)
+    def __init__(self, flags):
+        self.flags = flags
+        if self.flags.lr_type == 'step':
+            self.lr = StepLR(flags)
+        elif self.flags.lr_type == 'cos':
+            self.lr = CosLR(flags)
+        else:
+            print('Error, unsupported learning rate: ' + self.flags.lr_type)
+
+    def __call__(self, global_step):
+        return self.lr(global_step)
---
+++ tensorflow/script/test_learning_rate.py
@@ -0,0 +1,159 @@
+import unittest
+
+import numpy as np
+
+from learning_rate import *
+
+
+def make_flags():
+    from yacs.config import CfgNode as CN
+
+    flags = CN()
+    flags.max_iter = 160000  # Maximum training iterations
+    flags.lr_type = 'step'  # Learning rate type: step or cos
+    flags.learning_rate = 0.1  # Initial learning rate
+    flags.gamma = 0.1  # Learning rate step-wise decay
+    flags.step_size = (400,)  # Learning rate step size.
+    return flags
+
+
+class DatasetTest(tf.test.TestCase):
+
+    def test_lr(self):
+        flags = make_flags()
+        global_step = tf.Variable(0, trainable=False)
+        lr_factory = LRFactory(**flags)
+        self.assertTrue(isinstance(lr_factory.lr, StepLR))
+        self.assertEqual(lr_factory.lr.gamma, flags.gamma)
+        self.assertEqual(lr_factory.lr.step_size, flags.step_size)
+        self.assertEqual(lr_factory.lr.learning_rate, flags.learning_rate)
+        lr_step = lr_factory(global_step)
+
+        with tf.Session() as sess:
+            lr_current = sess.run(lr_step, feed_dict={global_step: 0})
+            self.assertEqual(np.float16(lr_current), np.float16(0.1))
+            lr_current = sess.run(lr_step, feed_dict={global_step: 400})
+            self.assertEqual(np.float16(lr_current), np.float16(0.1))
+
+            lr_current = sess.run(lr_step, feed_dict={global_step: 401})
+            self.assertEqual(np.float16(lr_current), np.float16(0.01))
+            lr_current = sess.run(lr_step, feed_dict={global_step: 800})
+            self.assertEqual(np.float16(lr_current), np.float16(0.01))
+
+            lr_current = sess.run(lr_step, feed_dict={global_step: 801})
+            self.assertEqual(np.float16(lr_current), np.float16(0.001))
+
+            lr_current = sess.run(lr_step, feed_dict={global_step: 5 * 400})
+            self.assertEqual(np.float16(lr_current), np.float16(0.00001))
+
+            lr_current = sess.run(lr_step, feed_dict={global_step: 6 * 400})
+            self.assertEqual(np.float16(lr_current), np.float16(0.000001))
+            lr_current = sess.run(lr_step, feed_dict={global_step: 7 * 400})
+            self.assertEqual(np.float16(lr_current), np.float16(0.000001))
+
+
+class test_cond(unittest.TestCase):
+
+    def test_cond1(self):
+        a = tf.constant(1)
+        b = tf.constant(-1)
+        res = tf.cond(tf.greater(a, b), true_fn=lambda: a, false_fn=lambda: b)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            result = sess.run(res)
+            assert result == 1
+
+    def test_cond2(self):
+        a = tf.constant(1)
+        b = tf.constant(-1)
+
+        with self.assertRaises(Exception) as context:
+            res = tf.cond(2 > 1, true_fn=lambda: a, false_fn=lambda: b)
+            with tf.Session() as sess:
+                sess.run(tf.global_variables_initializer())
+                sess.run(res)
+        self.assertTrue("pred must not be a Python bool" in str(context.exception))
+
+    def test_cond3(self):
+        a = tf.constant(1)
+        b = -1
+
+        res = tf.cond(tf.greater(a, b), true_fn=lambda: a, false_fn=lambda: b)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            result = sess.run(res)
+            self.assertEqual(result, 1)
+
+    def test_cond4(self):
+        a = tf.Variable(initial_value=0)
+        b = -1
+
+        a = tf.cond(tf.greater(a, b), true_fn=lambda: 10, false_fn=lambda: -10)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            result = sess.run(a)
+            self.assertEqual(result, 10)
+
+    def test_cond5(self):
+        a = tf.Variable(initial_value=0)
+        b = -1
+
+        a, b = tf.cond(tf.greater(a, b), true_fn=lambda: [-10, -20], false_fn=lambda: [10, 10])
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            resulta, resultb = sess.run([a, b])
+            self.assertEqual(resulta, -10)
+            self.assertEqual(resultb, -20)
+
+    def test_assign_instead_doesnt_work(self):
+        a = tf.Variable(initial_value=0)
+        b = -1
+
+        tf.cond(tf.greater(a, b),
+                true_fn=lambda: tf.assign(a, 10),
+                false_fn=lambda: tf.assign(a, 20))
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            resulta = sess.run(a)
+            self.assertEqual(resulta, 0)
+
+
+def make_plateau_flags():
+    from yacs.config import CfgNode as CN
+
+    flags = CN()
+    flags.mode = 'min'
+    flags.lr_type = 'plateau'  # Learning rate type: step or cos
+    flags.learning_rate = 0.1  # Initial learning rate
+    flags.patience = 0
+    flags.threshold_mode = 'rel'
+    flags.threshold = 1e-4
+    flags.cooldown = 0  # ####################
+    flags.min_lr = 0.0
+    flags.eps = 1e-8
+    flags.gamma = 0.1
+    return flags
+
+
+class OnPlateauLrTest(tf.test.TestCase):
+
+    def test_lr(self):
+        flags = make_plateau_flags()
+        lr_metric = OnPlateauLR(flags)
+        curr_metric = tf.Variable(0.2, trainable=False)
+        lr_value = lr_metric(curr_metric)
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            lr_current = sess.run(lr_value, feed_dict={curr_metric: 0.2})
+            print(lr_current)
+            lr_current = sess.run(lr_value, feed_dict={curr_metric: 0.4})
+            print(lr_current)
+            lr_current = sess.run(lr_value, feed_dict={curr_metric: 0.6})
+            print(lr_current)
+            lr_current = sess.run(lr_value, feed_dict={curr_metric: 0.1})
+            print(lr_current)
+
+
+if __name__ == "__main__":
+    tf.test.main()
