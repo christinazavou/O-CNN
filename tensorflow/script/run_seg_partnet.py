94db11c69bbc76560e7bc280253abb4148d34e21
migari <maslioukova@gmail.com@bitbucket.com>
2020-10-17 00:02:08 +0300

    Created file that transforms points, saves them in ocnn format and octree. Checked when only when octree was return, with octree and points needs more testing.

--- tensorflow/script/run_seg_partnet.py
+++ tensorflow/script/run_seg_partnet.py
@@ -350,13 +350,13 @@ class PartNetSolver(TFSolver):
                     # )
                     summary_writer.add_summary(summary_occ, i)
                     summary_writer.add_summary(summary_alw, i)
+                    print("self.lr: ")
+                    print(sess.run(self.lr))
                 else:
                     summary_alw, _, curr_loss, curr_lr = sess.run(
                         [self.summ_train_alw, self.train_op, self.total_loss, self.lr],
                         # feed_dict={self.lr: self.lr_metric(curr_loss)}
                     )
-                    print("self.lr: ")
-                    print(sess.run(self.lr))
                     summary_writer.add_summary(summary_alw, i)
                 # testing
                 if i % self.flags.test_every_iter == 0:
