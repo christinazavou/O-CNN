4f9a97ec3fc99ab3f46783f564d0df697f0cba51
christinazavou <czavou01@gmail.com>
2020-12-03 12:54:42 +0200

    refactored test about loss

--- tensorflow/script/tests/test_ocnn.py
+++ tensorflow/script/tests/test_ocnn.py
@@ -1,7 +1,103 @@
 import tensorflow as tf
 import numpy as np
 import pytest
+from scipy.special import softmax
+from numpy.random import randint
+
 from ocnn import loss_functions_seg
+
+
+def random_one_hot_encoding(N, C):
+    """ Return a random 2D one-hot vector based on N and C"""
+    arr = np.random.randint(low=0, high=C - 1, size=N)
+    one_hot = np.zeros((arr.size, C))
+    one_hot[np.arange(arr.size), arr] = 1
+    return one_hot
+
+
+def softmax_cross_entropy(onehot_labels, logits, reduction='sum'):
+    """Softmax Cross Entropy Numpy implementation"""
+    assert (onehot_labels.ndim >= 2)
+    assert (onehot_labels.shape == logits.shape)
+
+    # Apply softmax on logits
+    softmax_logits = softmax(logits, axis=-1)
+
+    # Reshape is needed
+    C = onehot_labels.shape[-1]
+    P = onehot_labels.view()
+    Q = softmax_logits.view()
+    P = np.reshape(P, (-1, C))
+    Q = np.reshape(Q, (-1, C))
+
+    # Calculate cross entropy
+    H = []
+    for p, q in zip(P, Q):
+        p_cond = p != 0
+        p_val = p[p_cond]
+        q_val = q[p_cond]
+        assert (q_val.size == 1)
+        H.append(-p_val * np.log(q_val))
+
+    # Return H based on reduction mode
+    H = np.squeeze(np.asarray(H))
+    if reduction == None:
+        return H
+    elif reduction == 'sum':
+        return np.sum(H)
+    elif reduction == 'mean':
+        return np.mean(H)
+    else:
+        print("Undefined reduction method {mthd:s}".format(mthd=reduction))
+        exit(-1)
+
+
+def test_tf_same_as_np():
+
+    with tf.Session() as sess:
+        for test_idx in range(5):
+            # Create 2D or 3D arrays/tensors
+            if np.random.random() < 0.5:
+                shape = (randint(low=5, high=100), randint(low=5, high=100), randint(low=5, high=100))
+            else:
+                shape = (randint(low=5, high=100), randint(low=5, high=100))
+
+            # Weighted or unweighted loss
+            weights = None
+            if np.random.random() < 0.5:
+                weights = np.random.rand(shape[-1]) * 10
+
+            # Declare gt labels in one-hot vector encoding and dummy network output (logits)
+            if len(shape) == 3:
+                onehot_labels = np.zeros(shape=shape, dtype=np.float32)
+                for batch_idx in range(shape[0]):
+                    labels = random_one_hot_encoding(N=shape[-2], C=shape[-1])
+                    if weights is not None:
+                        labels = np.multiply(labels, weights)
+                    onehot_labels[batch_idx] = labels
+            else:
+                onehot_labels = random_one_hot_encoding(N=shape[-2], C=shape[-1])
+                if weights is not None:
+                    onehot_labels = np.multiply(onehot_labels, weights)
+            logits = np.random.rand(*shape)
+            offset = np.random.randint(low=1, high=10)
+            logits = logits * offset - (offset / 2)
+
+            # tf loss
+            onehot_labels_pl = tf.placeholder(shape=shape, dtype=tf.float32, name='onehot_labels')
+            logits_pl = tf.placeholder(shape=shape, dtype=tf.float32, name='logits')
+            loss_op = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels_pl, logits=logits_pl,
+                                                      reduction='weighted_mean')
+            loss_op1 = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels_pl, logits=logits_pl,
+                                                       reduction='weighted_sum_by_nonzero_weights')
+            tf_loss = sess.run(loss_op, feed_dict={onehot_labels_pl: onehot_labels, logits_pl: logits})
+            tf_loss1 = sess.run(loss_op1, feed_dict={onehot_labels_pl: onehot_labels, logits_pl: logits})
+
+            # np loss
+            np_loss = softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits, reduction='mean')
+
+            assert (np.isclose(tf_loss, np_loss))
+            assert (np.isclose(tf_loss1, np_loss))
 
 
 def test_loss_functions_seg():
