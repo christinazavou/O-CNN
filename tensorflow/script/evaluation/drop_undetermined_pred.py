4c32df01496151409da2e596c404a39adbfd52ee
migari <maslioukova@gmail.com@bitbucket.com>
2020-12-18 13:32:37 +0200

    Drop undetermined probabilities from predictions

---
+++ tensorflow/script/evaluation/drop_undetermined_pred.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+
+IN_DIR = "/media/maria/BigData1/Maria/buildnet_data_2k/100K_inverted_normals/cluster_exp_results/Seg_2k_hrnet_w_colour_depth_5_unweighted_adam_8_0.5/best_iou/probabilities"
+assert (os.path.exists(IN_DIR))
+
+OUT_DIR = "/media/maria/BigData1/Maria/buildnet_data_2k/100K_inverted_normals/cluster_exp_results/Seg_2k_hrnet_w_colour_depth_5_unweighted_adam_8_0.5/best_iou/probabilities_no_und"
+
+if not os.path.exists(OUT_DIR):
+    os.mkdir(OUT_DIR)
+
+for root, _, files in os.walk(IN_DIR):
+    for f in files:
+        try:
+            x = np.load(os.path.join(root, f))
+        except IOError:
+            print("Cannot read file {}. Moving to next.".format(f))
+            continue
+        except ValueError:
+            print("Allowing pickle reading")
+            x = np.load(os.path.join(root, f), allow_pickle=True)
+        t = x[..., 1:]
+        t = np.true_divide(t, np.sum(t, axis=1)[:, None])
+        np.save(os.path.join(OUT_DIR, f), t)
